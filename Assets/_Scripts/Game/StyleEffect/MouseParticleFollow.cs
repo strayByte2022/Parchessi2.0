cf676cd9f1a5ca167ac8e705a106ace8e0118a7b
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-10-26 12:21:14 +0700

    feat: mouse particle effect

---
+++ Assets/_Scripts/Game/StyleEffect/MouseParticleFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Game.StyleEffect
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public class MouseParticleFollow : MonoBehaviour
+    {
+        private ParticleSystem _particleSystem;
+
+        private void Start()
+        {
+            _particleSystem = GetComponent<ParticleSystem>();
+            _particleSystem.Stop(); // Stop the particles initially
+        }
+
+        private void Update()
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0; // Ensure the same z-position as your camera
+
+            transform.position = mousePosition; // Set the particle system's position to the mouse cursor
+
+            if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+            {
+                _particleSystem.Play(); // Start the particle effect when the left mouse button is clicked
+            }
+            else if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1))
+            {
+                _particleSystem.Stop(); // Stop the particle effect when the left mouse button is released
+            }
+        }
+    }
+}
