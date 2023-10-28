38e82ea1f996b214206669883fbc8132de346cd5
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-10-28 20:51:57 +0700

    feat: pawn card

--- Assets/_Scripts/Game/Player/Card/HandCardVisual.cs
+++ Assets/_Scripts/Game/Player/Card/HandCardVisual.cs
@@ -11,19 +11,19 @@ public class HandCardVisual : MonoBehaviour
     protected HandCard HandCard;
     protected CardDescription CardDescription;
 
-    [SerializeField] private TMP_Text _cardName;
+    [SerializeField] protected TMP_Text _cardName;
     
     [Tooltip("Description template with placeholders for variables, e.g., 'Do {0} damage'")]
-    [SerializeField] private TMP_Text _cardEffectDescription;
-    [SerializeField] private SpriteRenderer _cardImage;
-    [SerializeField] private TMP_Text _cardCost;
+    [SerializeField] protected TMP_Text _cardEffectDescription;
+    [SerializeField] protected SpriteRenderer _cardImage;
+    [SerializeField] protected TMP_Text _cardCost;
 
-    [SerializeField] private SpriteRenderer _cardBorderSprite;
-    [SerializeField] private SpriteRenderer _cardEffectBoxSprite;
-    [SerializeField] private SpriteRenderer _cardBannerBoxSprite;
-    [SerializeField] private SpriteRenderer _cardImageBoxSprite;
+    [SerializeField] protected SpriteRenderer _cardBorderSprite;
+    [SerializeField] protected SpriteRenderer _cardEffectBoxSprite;
+    [SerializeField] protected SpriteRenderer _cardBannerBoxSprite;
+    [SerializeField] protected SpriteRenderer _cardImageBoxSprite;
     
-    [SerializeField] private SortingGroup _frontSortingGroup, _backSortingGroup;
+    [SerializeField] protected SortingGroup _frontSortingGroup, _backSortingGroup;
 
     protected virtual void Awake()
     {
@@ -58,7 +58,7 @@ public class HandCardVisual : MonoBehaviour
     
     protected virtual void LoadCardColor()
     {
-        if(CardDescription == null || CardDescription.CardPaletteDescription != null) return;
+        if(CardDescription == null || CardDescription.CardPaletteDescription == null) return;
         CardPaletteDescription cardPaletteDescription = CardDescription.CardPaletteDescription;
         
         _cardBorderSprite.color = cardPaletteDescription.CardBorderColor;
