2ad6c03fac86fce16ed729a9197a3b769d190a7d
VietLQ1 <vietdz145@gmail.com>
2023-10-28 23:26:02 +0700

    feat: script 301-305

---
+++ Assets/_Scripts/Game/CardScript/BeastCardScript/LastWordCard.cs
@@ -0,0 +1,77 @@
+using _Scripts.DataWrapper;
+using _Scripts.Managers.Game;
+using _Scripts.NetworkContainter;
+using _Scripts.Player.Pawn;
+using _Scripts.Scriptable_Objects;
+using _Scripts.Simulation;
+using UnityEngine;
+
+namespace _Scripts.CardScript.AquaticCardScript
+{
+    public class LastWordCard : StylizedHandCard
+    {
+        public ObservableData<int> DealDamage;
+
+        protected override void InitializeCardDescription(CardDescription cardDescription)
+        {
+            base.InitializeCardDescription(cardDescription);
+            
+        }
+
+        public override bool CheckTargeteeValid(ITargetee targetee)
+        {
+            if (targetee.TargetType == TargetType.Empty)
+            {
+                return true;
+            }
+
+            return GetAllEnemyPawnNotFullHealth(targetee);
+        }
+
+        private bool GetAllEnemyPawnNotFullHealth(ITargetee targetee)
+        {
+            if (targetee is MapPawn mapPawn)
+            {
+                return mapPawn.OwnerClientID != this.OwnerClientID && mapPawn.CurrentHealth.Value < mapPawn.MaxHealth.Value;
+            }
+
+            return false;
+        }
+
+        public override SimulationPackage ExecuteTargeter<TTargetee>(TTargetee targetee)
+        {
+            var package = new SimulationPackage();
+
+            if (targetee.TargetType != TargetType.Empty)
+            {
+                return package;
+            }
+
+            package.AddToPackage(HandCardFace.SetCardFace(CardFaceType.Front));
+            package.AddToPackage(MoveToMiddleScreen());
+
+            var selectedPawns = ActionManager.Instance.GetMapPawns(GetAllEnemyPawnNotFullHealth);
+
+            foreach (var pawn in selectedPawns)
+            {
+                package.AddToPackage(() =>
+                {
+                    // Inherit this class and write Card effect
+
+                    MapManager.Instance.TakeDamagePawnServerRPC(OwnerClientID, DealDamage.Value, pawn.ContainerIndex);
+                });
+            }
+
+            package.AddToPackage(() =>
+            {
+                PlayerCardHand.PlayCard(this);
+
+                Destroy();
+            });
+
+
+
+            return package;
+        }
+    }
+}
